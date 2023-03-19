9b723682717ba688ebedf765cf66abec4cf55088
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-03-19 14:01:33 +0900

    feat: read file instead hardcoded string

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
+use std::{env::args, fs::File, io::Read};
 use rnasm_lexer::Lexer;
 use rnasm_parser::Parser;
+use rnasm_report::report;
+use rnasm_span::Spannable;
 
 fn main() {
-    let lexer = Lexer::new("label: lda #$00");
-    let parser = Parser::new(lexer.lex().unwrap());
-    println!("{:#?}", parser.parse().unwrap());
+    let args = args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        eprintln!("abort");
+        return;
+    }
+
+    let mut file = File::open(&args[1]).unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+
+    let lexer = Lexer::new(input.clone());
+    let tokens = match lexer.lex() {
+        Ok(tokens) => tokens,
+        Err(e) => {
+            for e in e.iter() {
+                report(
+                    &input,
+                    e.span(),
+                    &args[1],
+                    "while lexing",
+                    &e.to_string()
+                );
+            }
+            return;
+        }
+    };
+    let parser = Parser::new(tokens);
+    let stmts = match parser.parse() {
+        Ok(stmts) => stmts,
+        Err(e) => {
+            for e in e.iter() {
+                report(
+                    &input,
+                    e.span(),
+                    &args[1],
+                    "while parsing",
+                    &e.to_string()
+                );
+            }
+            return;
+        }
+    };
+    println!("{:#?}", stmts);
 }
